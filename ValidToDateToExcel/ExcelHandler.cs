8e5c6fe73376f43bd1a27a6dc3f9a74d699f8757
Nipton <sillivan@yandex.ru>
2026-03-25 23:52:25 +0300

    Initial commit

---
+++ ValidToDateToExcel/ExcelHandler.cs
@@ -0,0 +1,120 @@
+﻿using ClosedXML.Excel;
+using System.Net;
+using System.Text.Json;
+
+
+namespace ValidToDateToExcel
+{
+    public class ExcelHandler
+    {
+        private readonly static HttpClient httpClient = new HttpClient( new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromMinutes(2)});
+        private readonly string filePath;
+        private readonly int[] yearsToCheck = { 2025, 2024, 2026, 2023, 2022 };
+        private static readonly Dictionary<string, string> modelMap = new()
+        {
+            { "ПУЛЬС", "\"ПУЛЬС\"" },
+            { "ПУЛЬС СТК", "Пульс СТК" },
+            { "ФОБОС 1", "ФОБОС 1" }
+        };
+        public ExcelHandler(string filePath) 
+        {
+            this.filePath = filePath;
+        }
+        static ExcelHandler()
+        {
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "MyExcelParser/1.0");
+        }
+        public async Task FillValidToDates()
+        {
+            using var wb = new XLWorkbook(filePath);
+            var ws = wb.Worksheet(1);
+            var rows = ws.RowsUsed().Skip(2);
+            int count = 0;
+            int total = rows.Count();
+            foreach (var row in rows)
+            {
+                count++;
+                WriteColored($"\rОбработка: {count}/{total}  ", ConsoleColor.Green);
+                string number = row.Cell("H").GetString();
+                string model = modelMap.TryGetValue(row.Cell("I").GetString(), out var mapped) ? mapped : "";
+                if (string.IsNullOrEmpty(number) || string.IsNullOrEmpty(model))
+                {
+                    WriteColored($"\nВ строке {row.RowNumber()} не удалось получить нормер или марку.", ConsoleColor.Red);
+                    continue;
+                }
+                var date = await GetValidToDate(number, model);               
+                if (date == null)
+                {
+                    WriteColored($"\nНе удалось получить дату для строки №{row.RowNumber()}", ConsoleColor.Red);
+                    continue;
+                }    
+                row.Cell("M").SetValue(date.Value);
+                row.Cell("M").Style.NumberFormat.Format = "dd.MM.yyyy";             
+            }
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string newFilePath = Path.Combine(desktopPath, fileName + "_result.xlsx");
+            wb.SaveAs(newFilePath);
+        }
+        private async Task<DateTime?> GetValidToDate(string number, string model)
+        {
+            foreach (var year in yearsToCheck)
+            {
+                string url = $"https://fgis.gost.ru/fundmetrology/eapi/vri?mi_number={Uri.EscapeDataString(number)}&year={year}&mit_notation={Uri.EscapeDataString(model)}&sort=verification_date+desc&rows=1";
+                int attempt = 1;
+                do
+                {
+                    try
+                    {
+                        using var response = await httpClient.GetAsync(url);
+                        if (response.StatusCode == HttpStatusCode.TooManyRequests)
+                        {
+                            Console.WriteLine($"Превышен лимит обращений к API. Ожидание. Попытка №{attempt}.");
+                            attempt++;
+                            await Task.Delay(2000);
+                            continue;
+                        }
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine($"Не удалось получить ответ на запрос для {number}, {year}");
+                            break;
+                        }
+                        var json = await response.Content.ReadAsStringAsync();
+                        using var doc = JsonDocument.Parse(json);
+                        int count = doc.RootElement.GetProperty("result").GetProperty("count").GetInt32();
+                        if (count > 0)
+                        {
+                            var item = doc.RootElement.GetProperty("result").GetProperty("items")[0];
+                            var dateString = item.GetProperty("valid_date").GetString();
+                            if(DateTime.TryParse(dateString, out var date))
+                            {
+                                return date;
+                            }
+                            Console.WriteLine($"Ошибка преобразования даты {dateString}");
+                            return null;
+                        }                        
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка в запросе с {number}, {year}: {ex.Message}. Попытка №{attempt}.");
+                        if (attempt <= 3)
+                            await Task.Delay(1000);
+                        attempt++;
+                    }
+                } while (attempt <= 3);
+                await Task.Delay(600);
+            }
+            return null;
+        }
+        private void WriteColored(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            if(color == ConsoleColor.Green)
+                Console.Write(message);
+            else 
+                Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ValidToDateToExcel/Program.cs
@@ -0,0 +1,73 @@
+﻿using DocumentFormat.OpenXml.Vml;
+using System.Threading.Tasks;
+
+namespace ValidToDateToExcel
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {           
+            Console.WriteLine("=== ValidToDateToExcel ===");
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1)Выбрать файл");
+                Console.WriteLine("0)Выход");
+                Console.Write("Ввод: ");
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        string? filePath = ShowOpenFileDialog();
+                        if (string.IsNullOrEmpty(filePath))
+                        {
+                            Console.WriteLine("Путь не указан!");
+                            continue;
+                        }
+                        else if (!File.Exists(filePath))
+                        {
+                            Console.WriteLine("Файл не найден!");
+                            continue;
+                        }
+                        Console.WriteLine("Выполнение.");
+                        try
+                        {
+                            var excelHandler = new ExcelHandler(filePath);
+                            await excelHandler.FillValidToDates();
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Обработка завершена.");
+                            Console.ResetColor();                           
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Ошибка {ex.Message}");
+                        }                       
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Неверный ввод.");
+                        break;
+                }                          
+            } while (true);
+        }
+        static string? ShowOpenFileDialog()
+        {
+            string? path = null;
+            var t = new Thread(() =>
+            {
+                using var dialog = new OpenFileDialog();
+                dialog.Title = "Выберите файл";
+                dialog.Filter = "Excel файлы|*.xlsx;*.xls";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    path = dialog.FileName;
+            });
+            t.SetApartmentState(ApartmentState.STA); 
+            t.Start();
+            t.Join(); 
+            return path;
+        }
+    }
+}
